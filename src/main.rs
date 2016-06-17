817a168a7457b2a2a55953ca7716f0c1f446293e
Jashank Jeremy <jashank@rulingia.com>
2016-06-17 20:43:51 +1000

    doing FreeBSD sysctl(3) with Rust
    there are two approaches to use here: the first, and probably the
    easiest, is the one I'm using here, with `sysctlbyname`, which takes a
    CString and a bunch of pointers and spews back sysctl data.
    
    the approach I'd like to use is something like
    
        let oid: [i32; 8] = [libc::CTL_HW, libc::HW_NCPU];
        let mut t: [i32; 1] = [0];
        let mut tlen: size_t = 4;
    
        unsafe {
            libc::sysctl(oid.as_ptr(), 2 as size_t,
    	    mem::transmute(&mut t), &mut tlen,
    	    ptr::null(), 0) }
    
    this didn't work, though, probably for reasons that'll become obvious
    the moment I try doing something else.
    
    stuff I haven't tried yet:
    
    - setting sysctl(3) values (doesn't really make sense in this context)
    - more complex structures
    
    - the Staff OIDs (0.x.*) for types, but that starts to smell like
      runtime type analysis and in any case the Staff OIDs are
      ~undocumented~ kernel interfaces
    
    woo hacks

--- src/main.rs
+++ src/main.rs
@@ -4,8 +4,10 @@ extern crate time;
 
 use std::time::Duration;
 use std::thread;
+use std::ffi::CString;
+use std::{ptr,mem};
 
-use libc::{c_int, c_double};
+use libc::{c_double, size_t};
 
 use rustc_serialize::json;
 
@@ -75,6 +77,59 @@ fn i3b_loadavg() -> I3BarBlock {
     }
 }
 
+fn coretemp_get_dev(i: usize) -> f32 {
+    let name = CString::new(format!("dev.cpu.{}.temperature", i)).unwrap();
+    let mut t: [i32; 1] = [0];
+    let mut tlen: size_t = 4;
+
+    unsafe {
+        libc::sysctlbyname(name.as_ptr(), mem::transmute(&mut t), &mut tlen, ptr::null(), 0)
+    };
+
+    // so, like,
+    // https://github.com/freebsd/freebsd/blob/master/sys/dev/coretemp/coretemp.c#L404
+    ((t[0] as f32)-2731f32) / 10f32
+}
+
+fn get_ncpu() -> usize {
+    // dunno why I can't do [CTL_HW, HW_NCPU]
+    let name = CString::new("hw.ncpu").unwrap();
+    let mut t: [i32; 1] = [0];
+    let mut tlen: size_t = 4;
+
+    unsafe {
+        libc::sysctlbyname(name.as_ptr(), mem::transmute(&mut t), &mut tlen, ptr::null(), 0)
+    };
+
+    t[0] as usize
+}
+
+fn i3b_bsd_coretemp() -> I3BarBlock {
+    let avg = {
+        let cpus = get_ncpu();
+
+        let mut roll = 0f32;
+        for c in 0..cpus {
+            roll += coretemp_get_dev(c);
+        }
+        roll /= cpus as f32;
+
+        roll
+    };
+
+    let col: String =
+        if avg >= 55f32 {
+            color(0xff, 0x00, 0x00)
+        } else {
+            color(0xff, 0xff, 0xff)
+        }.to_string();
+
+    I3BarBlock {
+        full_text: format!("{:.1} °C", avg).to_string(),
+        color: col
+    }
+}
+
 fn main() {
     let header: I3BarHeader = Default::default();
     println!("{}[", json::encode(&header).unwrap());
@@ -82,7 +137,7 @@ fn main() {
     let one_second = Duration::new(1, 0);
 
     loop {
-        let fns: Vec<fn() -> I3BarBlock> = vec![i3b_loadavg, i3b_clock];
+        let fns: Vec<fn() -> I3BarBlock> = vec![i3b_bsd_coretemp, i3b_loadavg, i3b_clock];
 
         let bar: Vec<I3BarBlock> = fns.iter().map(|&f| f()).collect();
         println!("{},", json::encode(&bar).unwrap());
@@ -90,4 +145,3 @@ fn main() {
         thread::sleep(one_second);
     }
 }
-
